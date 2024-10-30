3aaca56d55732b9f5bb25078cc5c22636b1e4f66
akzana <akzanaklepsch@outlook.com>
2024-10-29 22:17:07 -0700

    📱 💄 update `CommentForm` styling

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -4,13 +4,23 @@ import "./CommentForm.scss";
 export default function CommentForm() {
     return (
         <div className="comment-form">
-            <form action="submit" className="comment-form__form">
-                <img src="src\assets\images\Mohan-muruge.jpg" alt="user-image" className="comment-form__user-img" />
-                <div className='commenf-form__container'>
-                    <h3 className="comment-form__section-heading">JOIN THE CONVERSATION</h3>
-                    <input placeholder="Add a new comment" type="textarea" className="comment-form__user-input" id="comment-form__user-input" />
-                    <button className="comment-form__button">COMMENT</button>
+            <form
+                action="submit"
+                className="comment-form__form">
+                <div className='comment-form__container'>
+                    <img
+                        src="src\assets\images\Mohan-muruge.jpg"
+                        alt="Mohan Muruge side profile"
+                        className="comment-form__user-img" />
+                    <label htmlFor="comment-form__user-input" className="comment-form__input-label">JOIN THE CONVERSATION
+                        <input
+                            placeholder="Add a new comment"
+                            type="text"
+                            className="comment-form__user-input"
+                            id="comment-form__user-input" />
+                    </label>
                 </div>
+                <button className="comment-form__button">COMMENT</button>
             </form>
         </div>
     )
