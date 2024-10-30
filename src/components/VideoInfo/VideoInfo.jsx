bcd02f6566fc941fec4b0797d6b677ba42cc05dd
akzana <akzanaklepsch@outlook.com>
2024-10-29 20:08:34 -0700

    💄 update `video-info` style

--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -7,11 +7,11 @@ export default function VideoInfo({ videoInfoObj }) {
             <h1 className="video-info__heading">{videoInfoObj.title}</h1>
             <div className="video-info__container">
 
-                <div>
+                <div className="video-info__channel-info">
                     <p className="video-info__channel">By {videoInfoObj.channel}</p>
                     <p className="video-info__post-date">{new Date(videoInfoObj.timestamp).toLocaleDateString()}</p>
                 </div>
-                <div>
+                <div className="video-info__interaction-info">
                     <span>
                         <img src="src/assets/icons/views.svg" alt="views-icon" className="video-info__interaction-count-icon" /><p className="video-info__interaction-count">{videoInfoObj.views}</p>
                     </span>
@@ -21,7 +21,7 @@ export default function VideoInfo({ videoInfoObj }) {
                 </div>
             </div>
             <p className="video-info__description">{videoInfoObj.description}</p>
-            <p className="video-info__commment-count">{videoInfoObj.comments.length} Comments</p>
+            <p className="video-info__comment-count">{videoInfoObj.comments.length} Comments</p>
         </div>
     )
 }
