ce9dfb887473f7a966f95cf85cf215922785ebe0
akzana <akzanaklepsch@outlook.com>
2024-10-30 19:37:13 -0700

    complete `CommentSection` styling; update `VideoInfo` styling

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./CommentCard.scss";
 
 export default function CommentCard({ likes, time, name, comment }) {
     return (
         <div>
             <li className="comment">
-                <p className="comment__name">{name}</p>
-                <p className="comment__time">{new Date(time).toLocaleDateString()}</p>
-                <p className="comment__comment">{comment}</p>
-
+                <img src="" alt="user image" onerror="this.style.display='none'" className='comment__img comment__img--default' />
+                <div className="comment__container">
+                    <div className="comment__channel-info">
+                        <p className="comment__name">{name}</p>
+                        <p className="comment__time">{new Date(time).toLocaleDateString()}</p>
+                    </div>
+                    <p className="comment__comment">{comment}</p>
+                </div>
             </li>
         </div>
     )
--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import VideoCard from '../VideoCard/VideoCard';
 import "./VideoList.scss";
 
-export default function VideoList({ filterVideoList, handleSelectVideo}) {
+export default function VideoList({ filterVideoList, handleSelectVideo }) {
 
     return (
         <div>
             <ul className="video-list">
-            <h3 className="video-list__heading">NEXT VIDEOS</h3>
+                <h3 className="video-list__heading">NEXT VIDEOS</h3>
                 {filterVideoList.map((video) =>
                     <VideoCard
-                        key = {video.id}
-                        id = {video.id}
+                        key={video.id}
+                        id={video.id}
                         img={video.image}
                         title={video.title}
                         channel={video.channel}
